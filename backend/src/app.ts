9bbf76f5df2a7d4fbb7c87fb6459cae871104a5f
Rayala07 <rayala07viswanath@gmail.com>
2026-05-21 17:18:18 +0530

    api: Full ai workflow built

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,16 @@
 import express from "express";
-import useGraph from "./services/graph.ai.service.js";
+import runGraph from "./services/graph.ai.js";
 
 const app = express();
 
-app.get("/health", (req, res) => {
+app.get("/", async (req, res) => {
+  const result = await runGraph(
+    "Write a code for finding middle node in a Singly Linked List in js.",
+  );
+
   res.status(200).json({
-    status: true,
-    message: "Working.",
+    result,
   });
 });
-
-app.post("/use_graph", async (req,res) => {
-  await useGraph("What is the capital of France ?")
-})
 
 export default app;
